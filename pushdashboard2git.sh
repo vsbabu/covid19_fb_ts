1e0737391e310f456834e034b3254cdbf1e122c1
satheesh v <vsbabu@gmail.com>
2020-05-27 09:55:51 +0530

    Keep output separate first before switching branch

--- pushdashboard2git.sh
+++ pushdashboard2git.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+t=`mktmp -d`
+for f in *.png *.csv *.json *.html; do
+  mv $f $t/
+done
 git checkout gh-pages
+cp $t/*.* .
 git add *.png *.csv *.json *.html
 git commit -m "re-generated"
 git push
 git checkout master
+rm -fR $t
