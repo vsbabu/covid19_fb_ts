bfbd10e4befe0b66bee8044d568043ac7970d397
satheesh v <vsbabu@gmail.com>
2020-06-10 07:00:26 +0530

    hash is done on file before it is generated. fix

--- csv2dashboard.sh
+++ csv2dashboard.sh
@@ -11,14 +11,6 @@ CACHE_EXPIRY_TIME=`TZ=GMT date -d "+12 hours" +"%a, %d %b %Y %H:%M:%S %Z"`
 ## ideally, we should be using some html templating library to fill up the
 ## slots in the dashboard. for now, this will work just as well!
 rm -f o.html
-cat ${INPUT_FILE} | tail -n +2| python3 calendarmap.py -t $DATE_FORMAT -o ${INPUT_FILE}
-echo "<table><tr><th align=\"center\">confirmed</th><th align=\"center\">recovered</th><th align=\"center\">deceased</th></tr><tbody><tr>" >> o.html
-for CATEGORY in confirmed recovered deceased; do
-  hash=`md5sum "${INPUT_FILE}_pred_${CATEGORY}.html.png"|cut -f1 -d' '`
-  echo "<td width=\"33%\"><img src=\"${INPUT_FILE}_pred_${CATEGORY}.html.png?v=${hash}\" width=\"400\" height=\"200\" alt=\"${CATEGORY} predictions\"/></td>" >> o.html
-done
-echo "</tr></tbody></table>" >> o.html
-echo "<table><tr><td width=\"40%\" valign=\"top\" nowrap>" >> o.html
 for CATEGORY in confirmed recovered deceased; do
   #https://github.com/BurntSushi/xsv
   if [ "$CATEGORY" == "recovered" ]; then
@@ -27,11 +19,20 @@ for CATEGORY in confirmed recovered deceased; do
     COLOR_ARGS="-n lightgreen -p orange"
   fi
   xsv select dt,$CATEGORY ${INPUT_FILE} | tail -n +2| python3 weeklies.py -s $startdt -k $NUMWEEKS -o ${CATEGORY}.html  -g $INPUT_FILE -t $DATE_FORMAT  $COLOR_ARGS -m $MARKERS_YML
+done
+echo "<table><tr><th align=\"center\">confirmed</th><th align=\"center\">recovered</th><th align=\"center\">deceased</th></tr><tbody><tr>" >> o.html
+for CATEGORY in confirmed recovered deceased; do
+  hash=`md5sum "${INPUT_FILE}_pred_${CATEGORY}.html.png"|cut -f1 -d' '`
+  echo "<td width=\"33%\"><img src=\"${INPUT_FILE}_pred_${CATEGORY}.html.png?v=${hash}\" width=\"400\" height=\"200\" alt=\"${CATEGORY} predictions\"/></td>" >> o.html
+done
+echo "</tr></tbody></table>" >> o.html
+echo "<table><tr><td width=\"40%\" valign=\"top\" nowrap>" >> o.html
+for CATEGORY in confirmed recovered deceased; do
   cat ${CATEGORY}.html|sed  -e 's/ 00:00:00//g' -e 's/<table /<table border="0" cellspacing="2" cellpadding="2" /g' -e "s/<thead>/<caption style='font-weight:bold;background-color:#99ccff;'>${CATEGORY}<\/caption><thead>/g" -e 's/<td /<td width="60" align="right" /g' -e 's/<tr> *<th class="index_name level0" >.*<\/tr>//g' -e 's/nan//g' -e 's/<thead>.*<\/thead>//g' >> o.html
   rm -f ${CATEGORY}.html
 done
-# this is a bad html; so force an exit code of 0
 echo '</td><td valign="top">' >> o.html
+cat ${INPUT_FILE} | tail -n +2| python3 calendarmap.py -t $DATE_FORMAT -o ${INPUT_FILE}
 for f in ${INPUT_FILE}_0_*.png; do
   hash=`md5sum "$f"|cut -f1 -d' '`
   echo "<img src=\"${f}?v=${hash}\" /><br/>" >> o.html
