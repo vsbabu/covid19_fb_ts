89ea3a197493c3cd399c41449cd3d1ef19433b08
satheesh v <vsbabu@gmail.com>
2020-08-16 19:03:58 +0530

    Add navbar

--- pushdashboard2git.sh
+++ pushdashboard2git.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 t=`mktemp -d`
+#add a navbar
+barline="<p style=\"font-size:x-small;text-transform:uppercase;\">`ls *.html|sed 's/\(.*\).html/<a href="\1.html">\1<\/a>/g'|paste -s --delimiters="|"|sed 's/index</india</g'`</p>"
+for f in *.html; do
+  name=`echo $f|sed 's/.html//g'|sed 's/index/india/g'`
+  name=${name^^}
+  sed -i "s/>dashboard</>$name</g" $f
+  sed -i -e 's#<body \(.*\)>#<body \1><h2>'"${name}"'<\/h2>'"${barline}"'#g' $f
+done
 for f in *.png *.csv *.json *.html; do
   mv $f $t/
 done
